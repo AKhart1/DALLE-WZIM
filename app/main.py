1e2747678860549cd1514a931fdbf38a04869df2
VyvaHart <khartonyuk.v24@gmail.com>
2024-05-26 16:06:52 +0200

    Init web server

--- app/main.py
+++ app/main.py
@@ -1,42 +1,44 @@
-import json
+from flask import Flask, request, jsonify, render_template
 import os
 import requests
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
 
-app = FastAPI(
-    title="Dall-E WZIM",
-    version="1.0.0"
-)
+# Load environment variables from the .env file
+load_dotenv()
 
-templates = Jinja2Templates(directory="app/templates")
+app = Flask(__name__)
 
-@app.get("/get-image", response_class=HTMLResponse)
-def index(request: Request):
-    api_key = os.getenv("MY_SECRET")
-    
-    url = 'https://api.openai.com/v1/images/generations'
+# Get the OpenAI API key from the environment variables
+OPENAI_API_KEY = os.getenv("MY_SECRET", None)
+if not OPENAI_API_KEY:
+    raise ValueError("No API key found in environment variables.")
 
+OPENAI_API_URL = 'https://api.openai.com/v1/images/generations'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    prompt = request.form.get('prompt')
     headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {api_key}'
+        'Authorization': f'Bearer {OPENAI_API_KEY}',
+        'Content-Type': 'application/json'
     }
-
     data = {
-    'prompt': "Italy Sunset",
-    'model': "dalle-e-3",
-    'n': 1,  # Number of images to generate
-    'size': '1024x1024'  # Desired size of the generated image 
+        'prompt': prompt,
+        'n': 1,
+        'size': '512x512'
     }
-
-    print(f'The secret is: {api_key}')
-
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-
+    response = requests.post(OPENAI_API_URL, headers=headers, json=data)
+    response_json = response.json()
+    
     if response.status_code == 200:
-        response_data = response.json()
-        return templates.TemplateResponse("index.html", {"request": request, "response": response_data['data'][0]['url']})
+        image_url = response_json['data'][0]['url']
+        return jsonify({'image_url': image_url})
     else:
-        return templates.TemplateResponse("index.html", {"request": request, "response": "Image wasn't generated"})
+        return jsonify({'error': response_json}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
