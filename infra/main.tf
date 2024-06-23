3605116265e88075706052214ff6273e6fa025c1
dima999572 <dima999572.dubinevich@gmail.com>
2024-06-23 18:46:46 +0200

    Create, attach new IAM Role for the EC2 Instance

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "webapp_host" {
   ami           = "ami-0705384c0b33c194c"
   instance_type = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   user_data = file("${path.module}/scripts/ec2.sh") 
 
